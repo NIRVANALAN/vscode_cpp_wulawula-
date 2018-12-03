56db2d5d9a3c3a4a4d0e2cefc44c23cb239c0690
maver <maverickers@outlook.com>
2018-12-03 17:34:23 +0800

    'init'

---
+++ source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+main(int argc, char const *argv[])
+{
+    cout<<"grammar analysis program"<<endl;
+    // test code below
+    int a=1,b=1,c=3;
+    cout<<std::sqrt(a+c)<<endl;
+    system("pause");
+    return 0;
+}
